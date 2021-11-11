c079083186c6c56ba4a62f83c248e94c2ad0b821
Zach <zbayard@gmail.com>
2021-11-11 16:09:58 -0500

    updated main

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar.js';
 import Body from './components/MarketingCopy/MarketingCopy.js';
+import CallToAction from './components/CTA/CallToAction.js';
 
 const App = ()=>{
 
@@ -8,6 +9,7 @@ const App = ()=>{
     <div className='take-home'>
       <Navbar/>
       <Body/>
+      {/* <CallToAction/> */}
     </div>
   );
 };
--- src/components/CTA/CallToAction.js
+++ src/components/CTA/CallToAction.js
@@ -1,15 +1,14 @@
-import './MarketingCopy.css';
+import './CallToAction.css';
 
-const MarketingCopy = ()=>{
+const CallToAction = ()=>{
 
     const link = 'https://www.tacobell.com/food/cravings-value-menu?gclsrc=aw.ds&gclid=CjwKCAiAm7OMBhAQEiwArvGi3BCGRKRSqx-eL0YszoXdeOYMfHc4ir354g9d86THvfZ7yh9OUMi7pBoC6YkQAvD_BwE'
     return(
-        <div className='marketing-copy'>
-            <h1>marketing copy will go here</h1>
+        <div className='cta'>
             <div className="btn-box">
                 <a href={link} className="btn btn-color btn-animated">See cravings menu!</a>
             </div>
         </div>
     );
 };
-export default MarketingCopy;
+export default CallToAction;
